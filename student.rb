d8af991688e9c1049f9d0cec8ef2eaf11e3ef9c5
Nezerwa <eligrand2000@gmail.com>
2023-04-27 21:39:01 +0200

    add student subclass with classroom atrribute and play_hooky method

--- student.rb
+++ student.rb
@@ -1,9 +1,13 @@
-require './Person'
-
+require_relative 'person'
 class Student < Person
-  def initialize(classroom, age, name = 'Unknown', parent_permission: true)
-    super(age, name, parent_permission: parent_permission)
+  def initialize(age:, classroom:, name: 'Unknown', parent_permission: true)
+    super(age, name, parent_permission)
     @classroom = classroom
+  end
+
+  def classroom=(classroom)
+    @classroom = classroom
+    classroom.students << self
   end
 
   def play_hooky
