094c1f5f40b9179ddd37b2df302113afebdcd4d2
Nezerwa <eligrand2000@gmail.com>
2023-04-27 21:40:42 +0200

    add Teacher subclass with classroom atrribute and play_hooky method

--- teacher.rb
+++ teacher.rb
@@ -1,8 +1,9 @@
-require './person'
-
+require_relative 'person'
 class Teacher < Person
-  def initialize(specialization, age, name = 'Unknown', parent_permission: true)
-    super(age, name, parent_permission: parent_permission)
+  attr_accessor :specialization
+
+  def initialize(age:, specialization:, name: 'Unknown', parent_permission: true)
+    super(name, age, parent_permission)
     @specialization = specialization
   end
 
