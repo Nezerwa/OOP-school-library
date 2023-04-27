52a74193bc490cc7b949cfba15a33fa384bbb853
Nezerwa <eligrand2000@gmail.com>
2023-04-27 12:49:07 +0200

    Fix Linters

--- person.rb
+++ person.rb
@@ -25,35 +25,3 @@ class Person < Nameable
     @age >= 18
   end
 end
-require('./nameable')
-class Person < Nameable
-  attr_accessor :name, :age, :rentals
-  attr_reader :id
-
-  def initialize(age, name = 'Unknown', parent_permission: true)
-    super()
-    @id = Random.rand(1..1000)
-    @name = name
-    @age = age
-    @parent_permission = parent_permission
-    @rentals = []
-  end
-
-  def add_rental(date, book)
-    Rental.new(date, self, book)
-  end
-
-  def can_use_services?
-    of_age? || @parent_permission
-  end
-
-  def correct_name
-    @name
-  end
-
-  private
-
-  def of_age?
-    @age >= 18
-  end
-end
