6926fda363f90defdd825a576b0c292b3ba324c7
Nezerwa <eligrand2000@gmail.com>
2023-04-27 20:46:39 +0200

    Implement Requested Changes

--- person.rb
+++ person.rb
@@ -1,14 +1,14 @@
-require_relative './nameable'
-
+require_relative 'nameable'
 class Person < Nameable
-  attr_accessor :age
   attr_reader :id
+  attr_accessor :name, :age, :parent_permission
 
-  def initialize(age, name = 'Unknown', parent_permission: true)
-    super(name)
+  def initialize(age:, name: 'Unknown', parent_permission: true)
     @id = Random.rand(1..1000)
+    @name = name
     @age = age
     @parent_permission = parent_permission
+    super()
   end
 
   def can_use_services?
