902ffa561e62151c741ea1617de4264fbd17f116
Nezerwa <eligrand2000@gmail.com>
2023-05-01 18:14:57 +0200

    Add main app code and display menu for School library App

--- main.rb
+++ main.rb
@@ -1,50 +1,21 @@
-require_relative 'app'
+require('./app')
 
-def main
-  Main.new
+def display_list
+  puts 'Welcome to School library App!'
+  puts "
+  Choose an option by entering a number:
+  1 - List all books
+  2 - List all people
+  3 - Create a person
+  4 - Create a book
+  5 - Create a rental
+  6 - List all rentals for a given person id
+  7 - Exit"
 end
 
-class Main
-  puts 'Welcome to School Library OOP App!'
-  def self.menu
-    puts
-    puts 'Please choose an option by entering a number'
-    @list = {
-      1 => 'List all books',
-      2 => 'List all people',
-      3 => 'Create a person',
-      4 => 'Create a book',
-      5 => 'Create a rental',
-      6 => 'List all rentals for a given person id',
-      7 => 'Exit'
-    }
-    @list.each do |id, option|
-      puts "#{id}: #{option}"
-    end
-    gets.chomp.to_i
-  end
-  res = App.new
-  loop do
-    case menu
-    when 1
-      res.all_books
-    when 2
-      res.all_people
-    when 3
-      res.create_person
-    when 4
-      res.create_book
-    when 5
-      res.create_rental
-    when 6
-      res.list_rentals_of_person
-    when 7
-      puts 'Thank you for using this app!'
-      exit
-    else
-      puts 'Please choose a number between 1 and 7'
-    end
-  end
+def main
+  app = App.new
+  app.options
 end
 
 main
