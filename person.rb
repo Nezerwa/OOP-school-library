6a5a797d49778eec039d157b515e8cef4556a338
Nezerwa <eligrand2000@gmail.com>
2023-05-01 18:04:44 +0200

    Add Class with Nameable module and rental system

--- person.rb
+++ person.rb
@@ -1,15 +1,19 @@
-require_relative 'nameable'
+require('./nameable')
 class Person < Nameable
+  attr_accessor :name, :age, :rentals
   attr_reader :id
-  attr_accessor :name, :age, :parent_permission, :rentals
 
-  def initialize(age, name: 'Unknown', parent_permission: true)
+  def initialize(age, name = 'Unknown', parent_permission: true)
+    super()
     @id = Random.rand(1..1000)
     @name = name
     @age = age
     @parent_permission = parent_permission
     @rentals = []
-    super()
+  end
+
+  def add_rental(date, book)
+    Rental.new(date, self, book)
   end
 
   def can_use_services?
@@ -24,10 +28,5 @@ class Person < Nameable
 
   def of_age?
     @age >= 18
-  end
-
-  def add_rental(person, date = Date.today, book = self)
-    Rental.new(date, book, person)
-    @rentals << self unless @rentals.include?(self)
   end
 end
