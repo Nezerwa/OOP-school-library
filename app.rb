3c0c88a18df4edea1d46dc8bdaf9f7ff20109172
Lugard Agu <agulugard@gmail.com>
2023-05-01 17:42:47 +0100

    Add def options

--- app.rb
+++ app.rb
@@ -13,5 +13,28 @@ class App
     @list = List.new(@books, @people, @rentals)
     @create = Create.new(@books, @people, @rentals)
   end
+  def options
+    loop do
+      display_list
+      case gets.chomp.to_i
+      when 1
+        @list.list_all_books
+      when 2
+        @list.list_all_people
+      when 3
+        @create.create_person
+      when 4
+        @create.create_book
+      when 5
+        @create.create_rental
+      when 6
+        @list.list_all_rentals_by_id
+      else
+        break
+      end
+    end
+    display_list
+  end
+end
 
   
